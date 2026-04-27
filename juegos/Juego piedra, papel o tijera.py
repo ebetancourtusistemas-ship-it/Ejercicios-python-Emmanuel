106c679e5cfa9c509be1a7b96114b7091cd297e4
Emmanuel betancourt osorio <ebetancourt_usistemas@intep.edu.co>
2026-04-27 11:28:23 -0500

    tarea para hacer

---
+++ juegos/Juego piedra, papel o tijera.py
@@ -0,0 +1,42 @@
+"""
+Reto de programacion # 1: piedra, papel o tijera
+
+1. Descripción del problema:
+Se solicita desarrollar un programa interactivo en Python que permita a un usuario enfrentarse contra la computadora en el clásico juego de piedra, papel o tijera.
+El programa debe ser capaz de procesar la entrada del usuario, generar una respuesta aleatoria para determinar un ganador basandose en las reglas tradicionales.
+
+2. Requerimientos técnicos:
+El algoritmo debe estructurarse de la siguiente manera:
+
+*Interfaz Visual: Mostar un encabezado decorativo utilizando metodos de cadena como .center() y multuplicacion de caracteres.
+
+*Entrada de datos: Solicitar al usuario su elleccion. de be ser capaz de reconocer la entrada sin importar si se escribe en mayusculas o minusculas.
+
+*Inteligencia Aleatoria: La computadora debe elegir una opcion de una lista predefinida de opciones (piedra, papel o tijera) de forma aleatoria utilizando el modulo random.
+
+*Logica de comparacion:
+Implementar las condiciones necesarias para evaluar.
+
+1. Empate: Ambas elecciones son iguales.
+2. Victoria : El usuario vence a la PC.
+3. Derrota: La PC vence al usuario.
+
+* Control de flujo: El juego debe repetirse indefinidamente dentro de un bucle hasta que el usuario decida escribir la palabra (salir).
+
+
+
+Tarea para la casa, aparte del juego del papel, piedra o tijera debe programar y subir a github
+Debe programar una barra de carga que s eejecute durante 3 segundos
+
+Por medio de bucles imprime en la terminal las siguientes figuras geometricas:
+Triangulo
+Cuadrado
+Rectangulo
+Circulo
+Pentagono.
+
+CON CARACTERES ASCII OJOOÒ
+
+TEMA PARA CONSULTAR FUNCIONES EN PYTHON
+
+"""
